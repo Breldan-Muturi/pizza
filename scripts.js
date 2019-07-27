91d4e63d906f9a2294785da18c6aebbab8085b3a
Turi-byte <breldanmuturi001@gmail.com>
2019-07-27 22:07:00 +0300

    Add the buy button with its pop-up

--- scripts.js
+++ scripts.js
@@ -30,13 +30,22 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
+
 //Add active class to the current button(highlight it)
-var btnContainer=document.getElementById("myBtnContainer");
-var btns=btnContainer.getElementsByClassName("btn");
-for (var i=0; i<btns.length;i++){
-    btns[i].addEventListener("click",function(){
-        var current=document.getElementsByClassName("active");
-        current[0].className=current[0].className.replace(" active","");
-        this.className += " active";
-    });
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
 }
