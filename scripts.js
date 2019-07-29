f715ac2579c975db98788c73fc7a3475f1327923
Turi-byte <breldanmuturi001@gmail.com>
2019-07-29 10:30:19 +0300

    Adds the functionality for choosing size , crust and toppings

--- scripts.js
+++ scripts.js
@@ -48,190 +48,99 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
-function validation(id) {
-	var valid = true;
-	var quantity = $("#product-quantity-" + id).val();
-	if (quantity == "" || quantity == 0) {
-		valid = false;
+
+var totalOrderPrice=[];
+function Placeorder(size, crust, topping){
+	this.size=size;
+	this.crust=crust;
+	this.topping=topping;
+	this.price=0;
+}
+
+var pizzaSize=["Regular","Medium","Large"];
+var pizzaCrust = ["crispy","stuffed","gluten-free"];
+var toppings=["cheese","pepperoni","pineapple","onion"];
+
+Placeorder.prototype.costOfPizza=function(){
+	if (this.size === pizzaSize[0]){
+		this.price += 1000;
+	} else if(this.size===pizzaSize[1]){
+		this.price+= 1500;
+	} else if (this.size===pizzaSize[2]){
+		this.price+= 2000;
 	}
-	return valid;
-}
-
-function checkOutShowBox() {
-	$("#paynow-container").slideToggle();
-}
-
-function paynowValidation() {
-	var valid = true;
-	var customer_name = $("#customer_name").val();
-	var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	$("#customer_name").css("border", "#2ecc71 1px solid");
-	$("#error").html("").hide();
-	if (customer_name == "" || customer_name.trim() == "") {
-		$(".error").html(" required.").css({
-			"color" : "#d96557",
-			"margin" : "5px"
-		}).show();
-		$("#customer_name").css("border", "#d96557 1px solid");
-		valid = false;
+	if (this.crust === pizzaCrust[0]){
+		this.price += 0;
+	} else if(this.crust===pizzaCrust[1]){
+		this.price+= 0;
+	} else if (this.crust===pizzaCrust[2]){
+		this.price+= 0;
 	}
-	if (customer_name != "" && customer_name.trim() != ""
-			&& !emailRegex.test(customer_name)) {
-		$("#customer_name").css("border", "#d96557 1px solid");
-		$("#error").html("Invalid email address.").css({
-			"color" : "#d96557",
-			"margin" : "5px"
-		}).show();
-		valid = false;
+	if (this.topping === toppings[0]){
+		this.price += 150;
+	} else if(this.topping===toppings[1]){
+		this.price+= 100;
+	} else if (this.topping===toppings[2]){
+		this.price+= 100;
 	}
-	return valid;
+	return this.price;
 }
-
-function checkOut(cartCount) {
-	if (cartCount > 1) {
-		var valid = paynowValidation();
-		if (valid == true) {
-			var customerEmail = $("#customer_name").val();
-			$("#paynow-btn").hide();
-			$("#paynow-loader").show();
-			$.ajax({
-				url : "?checkout=check&mail=" + customerEmail,
-				type : "GET",
-				dataType : "html", // expect html to be returned
-				success : function(response) {
-					window.location.replace("?checkout=check-success");
-					$("#paynow-btn").show();
-					$("#paynow-loader").hide();
-				},
-				error : function(response) {
-					window.location.replace("?checkout=check-error");
-					console.log("#paynow-btn");
-				}
-			});
-		}
+Placeorder.prototype.totalCost = function() {
+ 
+	var shoppingCartTotal = 0;
+	
+	for (var order = 0; order < totalOrderPrice.length; order++) {
+	
+	  shoppingCartTotal += totalOrderPrice[order];
 	}
-}
-
-function deleteBySingleProduct(cartCount, index, id) {
-	if (cartCount >= 1) {
-		$.ajax({
-			url : "?checkout=delete-single&index=" + index,
-			success : function(response) {
-				$("#tick-icon-" + id).hide();
-				$("#add-to-button-" + id).show();
-				$(".checkout-title-row").html(response);
-			}
-		});
-	}
-}
-
-function addToCart(id) {
-	var valid = validation(id);
-	if (valid == true) {
-		$("#add-to-cart-button-" + id).hide();
-		$("#adding_load_" + id).removeClass("loader-hide");
-		var product_id = $('#product-id-' + id).val();
-		var quantity = $('#product-quantity-' + id).val();
-		var product_title = $('#title-' + id).val();
-		var product_price = $('#price-' + id).val();
-
-		var query = "product_id=" + product_id + "&product_title="
-				+ product_title + "&product_quantity=" + quantity
-				+ "&product_price=" + product_price;
-		if ($('#img-url-' + id).length > 0) {
-			query += "&image_url=" + $('#img-url-' + id).val();
-		}
-		$.ajax({
-			type : 'POST',
-			data : query,
-			url : "hera/ajax-end-point/add-to-cart-endpoint.php?cart=add",
-			success : function(response) {
-				$("#adding_load_" + id).addClass("loader-hide");
-				$("#added_btn_" + id).removeClass("loader-hide");
-				var productResp = $.parseJSON(response);
-				$('#cart_counts').html(productResp[0]['count']);
-				$('.display-message-foundation').remove();
-				$('.display-success').remove();
-				if (!document.getElementById('#gotocart_btn')) {
-					$("#gotocart_btn").remove();
-					$('#layout-sec').append(productResp[0]['goCart']);
-					$("#gotocart_btn").delay(2000).fadeOut("slow");
-				}
-			}
-		});
-	}
-}
-
-function buyNow(id) {
-	var valid = validation(id);
-	if (valid == true) {
-		var product_id = $('#product-id-' + id).val();
-		var quantity = $('#product-quantity-' + id).val();
-		var product_title = $('#title-' + id).val();
-		var product_price = $('#price-' + id).val();
-		var imageurl = $('#img-url-' + id).val();
-		var redirectUrl = $('#redirect-url-' + id).val();
-		var query = "product_id=" + product_id + "&product_title="
-				+ product_title + "&product_quantity=" + quantity
-				+ "&product_price=" + product_price;
-		if ($('#img-url-' + id).length > 0) {
-			query += "&image_url=" + $('#img-url-' + id).val();
-		}
-		$.ajax({
-			type : 'POST',
-			data : query,
-			url : "hera/ajax-end-point/buynow-endpoint.php?cart=add",
-			success : function(response) {
-				$('#cart_counts').html(response);
-				window.location.replace(redirectUrl);
-
-			}
-		});
-	}
-}
-
-function increment_quantity(id) {
-	var inputQuantityElement = $("#" + id);
-	var newQuantity = parseInt($(inputQuantityElement).val()) + 1;
-	$(inputQuantityElement).val(newQuantity);
-}
-
-function decrement_quantity(id) {
-	var inputQuantityElement = $("#" + id);
-	if ($(inputQuantityElement).val() > 1) {
-		var newQuantity = parseInt($(inputQuantityElement).val()) - 1;
-		$(inputQuantityElement).val(newQuantity);
-	}
-}
-
-function updateQuantity() {
-	var valid = true;
-	var regex = /\./;
-
-	$('.quantity-input').each(
-			function() {
-				if (!$.isNumeric($(this).val()) || $(this).val() == ""
-						|| $(this).val() == 0 || regex.test($(this).val())
-						|| $(this).val() < $(this).data("min")) {
-					$($(this)).css("border", "1px solid #f59494");
-
-					valid = false;
-				}
-			});
-
-	if (valid == true) {
-		var data = $('#frm-cart').serialize();
-		$.ajax({
-			type : 'POST',
-			data : data,
-			url : "index.php?cart=update",
-			success : function(response) {
-				$(".checkout-title-row").html(response);
-			}
-		});
-	}
-}
-
-function hideCart() {
-	$("#cartHide").slideToggle();
-}
+	return shoppingCartTotal;
+  }
+  $(document).ready(function() {
+	$(".submit").click(function() {
+	  $(".contacts").toggle(2000);
+	  
+	});
+	$(".additional-info").hide();
+   
+	
+  
+	$("form#customized-pizza").submit(function(event) {
+	  event.preventDefault();
+	  var customSize = $("select#size option:selected").val();
+	  var customcrust = $("select#crust option:selected").val();
+	  var customtopping = $("select#topping option:selected").val();
+	  var newPizzaOrder = new Placeorder(customSize , customcrust , customtopping);
+	  
+	  // var totalOrderPrice = customSize + customcrust + customtopping;
+  
+	  newPizzaOrder.costOfPizza();
+  
+	  totalOrderPrice.push(newPizzaOrder.price);
+  
+  
+  
+	  $("#pizza-size").show();
+	  $("#pizza-crust").show();
+	  $("#pizza-topping").show();
+	  $("#pizza-size").append("\t" + "\t" + customSize);
+	  $("#pizza-crust").append("\t" + "\t" + customcrust);
+	  $("#pizza-topping").append("\t" + "\t" + customtopping);
+	  $("#final-cost").text("\t" + "\t" + newPizzaOrder.totalCost());
+	  
+	  var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $("#pizza-size").text() + " - " + customSize + '</td><td id="crust">' + $("#pizza-crust").text() + " - " + customcrust + '</td><td id="topping">' + $("#pizza-topping").text() + " - " + customtopping + '</td><td id="final-cost">' + $("#final-cost").text()+" - "+ newPizzaOrder.totalCost() + '</td></tr>'
+  
+		$("#pizza").append(newRow);
+	});
+	$("#checkout-btn").click(function(event) {
+	  event.preventDefault();
+	  // location.reload(); 
+	  $(".additional-info").show();
+	  $(".additional-info h3 span").html(newPizzaOrder.totalCost());
+	  
+	});
+	$("#reload").click(function(){
+	  location.reload();
+	});
+  
+	
+  });
